8999c4bfe0dbf3e5824a61b5e603f75532616863
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-02-17 15:24:21 -0800

    Handling redirect url from url_new

--- express_server.js
+++ express_server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+
+function generateRandomString(leng) {
+  return Math.random()
+    .toString(36)
+    .substring(2, 2 + leng);
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -13,9 +22,25 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
