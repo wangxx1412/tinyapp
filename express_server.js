69b785901b2d5cd37b2f342c8f612e37f38a5af6
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-02-17 18:36:37 -0800

    Add cookies for login/logout

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
@@ -35,7 +38,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -73,6 +76,17 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: "Hello World" };
   res.render("hello_world", templateVars);
+});
+
+// login
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+//logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.get("/set", (req, res) => {
