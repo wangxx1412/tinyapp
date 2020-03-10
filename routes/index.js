96d330979318a263a0a2c876974a27c0432631c5
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-03-09 17:25:35 -0700

    tiny change

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.use(methodOverride("_method"));
 let users = {};
 
 const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJs48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
